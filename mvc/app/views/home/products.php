7dffa4daa3734efea56a9929fc142c5aaa5b3940
adriantiron <tda_mda@yahoo.com>
2018-06-14 21:21:33 +0300

    Fixed feed

--- mvc/app/views/home/products.php
+++ mvc/app/views/home/products.php
@@ -6,22 +6,19 @@
 
 <link rel="stylesheet" href="resources/stylesheets/products.css" type="text/css"/>
 
-<main>
-    <div class="wrapper">
-        <section class="index-links">
 
-
-            <?php $products = $data['products'];
+            <?php 
+            $products = $data['products'];
 
 
             foreach ($products as $product) {
-                echo '<div class="product" >';
+                echo '<div class="product">';
                     echo '<a href="product_page">';
                         echo '<h2>'. $product['title'] .  ' [' . $product['condition'] . ']</h2>';
-                        echo '<img src="resources/images/' . $product['image'] . '" alt="alternative">';
+                        echo '<img src="resources/images/' . $product['image'] . '" alt="alternative" />';
                     echo '</a>';
 
-                    echo '<div class=rightSide">';
+                    echo '<div class="rightSide">';
                         echo '<div id="desc" class="desc" onclick="coll()">';
                             echo '<p><strong>DESCRIPTION</strong>:</p>';
                             echo $product['description'];
@@ -48,41 +45,39 @@
             }
 
             ?>
-        </section>
+
+
+<!--
+<div class="product" >
+    <a href="#">
+        <h2>Speakers [Good]</h2>
+        <img src="resources/images/speakers.jpg" alt="alternative" />
+    </a>
+
+    <div class="rightSide">
+        <div id="desc" class="desc" onclick="coll()">
+            <p><strong>DESCRIPTION</strong>:</p>
+            This is some long text that will not fit in the box. This is some long text that will not fit in the box.
+            This is some long text that will not fit in the box. This is some long text that will not fit in the box.
+            This is some long text that will not fit in the box. This is some long text that will not fit in the box.
+            This is some long text that will not fit in the box. This is some long text that will not fit in the box.
+            This is some long text that will not fit in the box. This is some long text that will not fit in the box.
+        </div>
+        <div class="inner">
+            <p><strong>Brand</strong>: Shure</p>
+            <p><strong>Country of provenance</strong>: United States</p>
+            <p><strong>Keywords</strong>: audio device</p>
+            <p><strong>Expiration Date</strong>: 2018-06-17</p>
+        </div>
+        <div class="prices">
+            <p>Current Price - 200$</p>
+            <p>Next Price - 220$</p>
+         </div>
+        <button class="button" style="vertical-align:middle"><span>Bid </span></button>
     </div>
-</main>
-
-<!---->
-<!--<div class="product" >-->
-<!--    <a href="#">-->
-<!--    	<h2>Speakers [Good]</h2>-->
-<!--        <img src="resources/images/speakers.jpg" alt="alternative" />-->
-<!--    </a>-->
-<!---->
-<!--    <div class="rightSide">-->
-<!--	    <div id="desc" class="desc" onclick="coll()">-->
-<!--	    	<p><strong>DESCRIPTION</strong>:</p>-->
-<!--	        This is some long text that will not fit in the box. This is some long text that will not fit in the box.-->
-<!--	        This is some long text that will not fit in the box. This is some long text that will not fit in the box.-->
-<!--	        This is some long text that will not fit in the box. This is some long text that will not fit in the box.-->
-<!--	        This is some long text that will not fit in the box. This is some long text that will not fit in the box.-->
-<!--	        This is some long text that will not fit in the box. This is some long text that will not fit in the box.-->
-<!--	    </div>-->
-<!--	    <div class="inner">-->
-<!--	    	<p><strong>Brand</strong>: Shure</p>-->
-<!--	    	<p><strong>Country of provenance</strong>: United States</p>-->
-<!--	        <p><strong>Keywords</strong>: audio device</p>-->
-<!--	        <p><strong>Expiration Date</strong>: 2018-06-17</p>-->
-<!--	    </div>-->
-<!--	    <div class="prices">-->
-<!--		    <p>Current Price - 200$</p>-->
-<!--		    <p>Next Price - 220$</p>-->
-<!--		 </div>-->
-<!--	    <button class="button" style="vertical-align:middle"><span>Bid </span></button>-->
-<!--	</div>-->
-<!--	<br style="clear:both;"/>-->
-<!--</div>-->
-
+    <br style="clear:both;"/>
+</div>
+-->
 
 <?php
 
